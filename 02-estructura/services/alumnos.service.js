9d543b8879720c6940b4d209b4d2724545078286
Preda <predaomfg@gmail.com>
2025-10-08 13:47:08 +0200

    feat: controller and service from alumnos

---
+++ 02-estructura/services/alumnos.service.js
@@ -0,0 +1,14 @@
+
+import { alumnos } from "../db/memory.js";
+
+// En un servicio real, 
+// aquí irían las llamadas a la base de datos
+// Las consultas a la base de datos son operaciones asíncronas
+// por lo que normalmente devolverían una promesa
+// y habría que usar async/await
+
+export const getAlumnosFromDb = () => {
+
+    // SELECT * FROM users;
+    return alumnos;
+}

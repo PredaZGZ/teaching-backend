55b324f51cd3b8617b1ef01dd0e55779a000847a
Preda <predaomfg@gmail.com>
2025-10-08 13:57:44 +0200

    feat: controller and service from users (missing auth logic)

--- 02-estructura/routes/auth.routes.js
+++ 02-estructura/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { getUsuarios } from '../controllers/usuarios.controller.js';
+
 const router = Router();
 
 router.get('/users', (req, res) => {
-    res.send({
-        "users": []
-    })
+    
+    const usuarios = getUsuarios();
+    res.send(usuarios);
+
 });
 
 export default router;
---
+++ 02-estructura/services/usuarios.service.js
@@ -0,0 +1,8 @@
+
+import { users } from "../db/memory.js";
+
+export const getUsuariosFromDb = () => {
+
+    // SELECT * FROM users;
+    return users;
+}

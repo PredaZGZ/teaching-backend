7e54487f3b7ba59ba012a1153eca6973b9c5bd90
Preda <predaomfg@gmail.com>
2025-10-17 11:10:41 +0200

    03-crud: Estructura inicial

---
+++ 03-crud/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+import logger from './middlewares/logger.js';
+
+import mainRouter from './routes/mainRouter.js';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Middleware para entender JSON 
+app.use(express.json());
+
+// Middleware de logueo
+app.use(logger);
+
+// Endpoint raiz
+app.get('/', (req, res) => { 
+    res.send({
+        "mensaje": 'Hola mundo desde mi primer servidor con Express'
+    })
+})
+
+// Cargar rutas
+app.use('/api', mainRouter);
+
+// Escuchar en el puerto 80
+app.listen(PORT, () => {
+    console.log('Servidor escuchando en el puerto: ' + PORT);
+})
+

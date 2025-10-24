5ef6c13d3c6d7f6ab11016030a363a54be5688fc
Preda <predaomfg@gmail.com>
2025-10-24 10:47:30 +0200

    logger

--- 03-crud/middlewares/logger.js
+++ 03-crud/middlewares/logger.js
@@ -4,29 +4,35 @@ import path from 'path';
 // Export permite importarlo en otros ficheros
 
 const logger = (req, res, next) => {
+    const startTime = Date.now();
     const today = new Date();
-
-    const string = `[${today.toISOString()}] ${req.method} - ${req.url} - ${req.socket.remoteAddress} `
-    console.log(string);
-    
     const date = today.toISOString().slice(0,10);
-
-    let logPath = path.resolve('./logs/');
     
-    if(!fs.existsSync(logPath)) {
-        fs.mkdirSync(logPath);
-    }    
+    // Listen for when the response is finished
+    res.on('finish', () => {
+        const endTime = Date.now();
+        const duration = endTime - startTime;
+        
+        const string = `[${today.toISOString()}] ${req.method} - ${req.url} - ${req.socket.remoteAddress} - ${duration}ms`;
+        console.log(string);
+        
+        let logPath = path.resolve('./logs/');
+        
+        if(!fs.existsSync(logPath)) {
+            fs.mkdirSync(logPath);
+        }    
 
-    logPath = path.join(logPath, date + '.log');
+        logPath = path.join(logPath, date + '.log');
 
-    fs.appendFile(logPath, 
-        string + '\n',
-        (error) => {
-            if (error) {
-                console.log(error);
+        fs.appendFile(logPath, 
+            string + '\n',
+            (error) => {
+                if (error) {
+                    console.log(error);
+                }
             }
-        }
-    );
+        );
+    });
     
     next();
 }
